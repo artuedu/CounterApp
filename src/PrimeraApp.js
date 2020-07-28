16078925525ead5ccc759df34425ba5e1c337a8f
Arturo Gómez <artuedu@gmail.com>
2020-07-27 20:17:59 -0500

    Commit inicial

---
+++ src/PrimeraApp.js
@@ -0,0 +1,29 @@
+// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+//Functional Component
+const PrimeraApp = ({saludo, subtitulo}) => {
+
+    return (
+        // <Fragment   >
+        //     <h1>Hola Mundo</h1>
+        //     <p>Mi primera aplicacion</p>
+        // </Fragment  >
+        <>
+            <h1>{saludo}</h1>
+            {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
+            <p>{subtitulo}</p>
+        </>
+    );
+
+}
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+export default PrimeraApp;
